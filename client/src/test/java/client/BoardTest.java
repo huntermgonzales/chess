927f052cbc3d1598644528ff9dde76dd59ee441f
huntermgonzales <huntermgonzales@gmail.com>
2024-11-14 19:19:34 -0700

    phase 5 code quality 2

--- client/src/test/java/client/BoardTest.java
+++ client/src/test/java/client/BoardTest.java
@@ -15,16 +15,4 @@ public class BoardTest {
         System.out.print(drawing);
     }
 
-    @Test
-    void pseudoDraw() {
-        ChessBoardArtist artist = new ChessBoardArtist();
-        ChessBoard board = new ChessBoard();
-        board.resetBoard();
-        for (int i = 1; i <= 8; i++) {
-            for (int j = 1; j <= 8; j++) {
-                System.out.print(artist.getPieceString(board, i, 8 - j));
-            }
-            System.out.print("\n");
-        }
-    }
 }
