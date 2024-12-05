50cc38bf4f5d9e883f94071234bf528cac1cbe60
huntermgonzales <huntermgonzales@gmail.com>
2024-12-05 00:36:07 -0700

    phase 6 removed repeated code in highlight board

--- client/src/main/java/ui/ChessBoardArtist.java
+++ client/src/main/java/ui/ChessBoardArtist.java
@@ -2,7 +2,6 @@ package ui;
 
 import chess.*;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
 
@@ -32,11 +31,15 @@ public class ChessBoardArtist {
 
     private String squareShade;
 
-    private void highlightSquare() {
-        if (Objects.equals(squareShade, EscapeSequences.SET_BG_COLOR_LIGHT_GREY)) {
-            squareShade = EscapeSequences.SET_BG_COLOR_GREEN;
-        } else {
-            squareShade = EscapeSequences.SET_BG_COLOR_DARK_GREEN;
+    private void determineHighlight(Collection<ChessMove> moves, int row, int col) {
+        for (ChessMove move : moves) {
+            if (Objects.equals(move.getEndPosition(), new ChessPosition(row, 9 - col))) {
+                if (Objects.equals(squareShade, EscapeSequences.SET_BG_COLOR_LIGHT_GREY)) {
+                    squareShade = EscapeSequences.SET_BG_COLOR_GREEN;
+                } else {
+                    squareShade = EscapeSequences.SET_BG_COLOR_DARK_GREEN;
+                }
+            }
         }
     }
 
@@ -62,7 +65,9 @@ public class ChessBoardArtist {
         return drawing.toString();
     }
 
-    public String drawBoard(ChessBoard board, ChessGame.TeamColor perspective) {
+
+    public String renderBoard(ChessBoard board, ChessGame.TeamColor perspective,
+                              ChessGame game, Collection<ChessMove> moves) {
         String borderColor = EscapeSequences.SET_BG_COLOR_MAGENTA;
         squareShade = EscapeSequences.SET_BG_COLOR_LIGHT_GREY;
         int inc;
@@ -83,6 +88,9 @@ public class ChessBoardArtist {
             drawing.append(EscapeSequences.SET_BG_COLOR_MAGENTA + EscapeSequences.SET_TEXT_COLOR_WHITE + " ").
                     append(row).append(" ").append(EscapeSequences.RESET_BG_COLOR);
             for (int col = start; col <= 8 && col >= 1; col += inc) {
+                if (moves != null) {
+                    determineHighlight(moves, row, col);
+                }
                 drawing.append(squareShade).append(getPieceString(board, row, 9 - col)).append(EscapeSequences.RESET_BG_COLOR);
                 changeShade();
             }
@@ -95,51 +103,59 @@ public class ChessBoardArtist {
         drawing.append(EscapeSequences.RESET_BG_COLOR);
 
         return drawing.toString();
+
+    }
+    public String drawBoard(ChessBoard board, ChessGame.TeamColor perspective) {
+        return renderBoard(board, perspective, null, null);
     }
 
     public String highlightBoard(ChessGame game, ChessGame.TeamColor perspective, ChessPosition chessPosition) {
         ChessBoard board = game.getBoard();
-        String borderColor = EscapeSequences.SET_BG_COLOR_MAGENTA;
-        squareShade = EscapeSequences.SET_BG_COLOR_LIGHT_GREY;
         Collection<ChessMove> moves = game.validMoves(chessPosition);
+        return renderBoard(board, perspective, game, moves);
 
-        int inc;
-        int start;
-        if (perspective != ChessGame.TeamColor.BLACK) {
-            inc = -1;
-            start = 8;
-        } else {
-            inc = 1;
-            start = 1;
-        }
-
-        StringBuilder drawing = new StringBuilder();
-        drawing.append(drawTopOrBottom(perspective));
-        for (int row = start; row <=8 && row >=1; row += inc) {
-            //draws the left border
-            drawing.append(EscapeSequences.SET_BG_COLOR_MAGENTA + EscapeSequences.SET_TEXT_COLOR_WHITE + " ").
-                    append(row).append(" ").append(EscapeSequences.RESET_BG_COLOR);
-            for (int col = start; col <= 8 && col >= 1; col += inc) {
-                //figures out if square needs to be shaded or not
-                for (ChessMove move : moves) {
-                    if (Objects.equals(move.getEndPosition(), new ChessPosition(row, 9 - col))) {
-                        highlightSquare();
-                    }
-                }
-
-                drawing.append(squareShade).append(getPieceString(board, row, 9 - col)).append(EscapeSequences.RESET_BG_COLOR);
-                changeShade();
-            }
-            //draws the right border
-            drawing.append(borderColor).append(" ").append(row).append(" ").append(EscapeSequences.SET_BG_COLOR_BLACK).append("\n");
-            changeShade();
-        }
-        drawing.append(borderColor);
-        drawing.append(drawTopOrBottom(perspective));
-        drawing.append(EscapeSequences.RESET_BG_COLOR);
-
-
-        return drawing.toString();
+//        ChessBoard board = game.getBoard();
+//        String borderColor = EscapeSequences.SET_BG_COLOR_MAGENTA;
+//        squareShade = EscapeSequences.SET_BG_COLOR_LIGHT_GREY;
+//        Collection<ChessMove> moves = game.validMoves(chessPosition);
+//
+//        int inc;
+//        int start;
+//        if (perspective != ChessGame.TeamColor.BLACK) {
+//            inc = -1;
+//            start = 8;
+//        } else {
+//            inc = 1;
+//            start = 1;
+//        }
+//
+//        StringBuilder drawing = new StringBuilder();
+//        drawing.append(drawTopOrBottom(perspective));
+//        for (int row = start; row <=8 && row >=1; row += inc) {
+//            //draws the left border
+//            drawing.append(EscapeSequences.SET_BG_COLOR_MAGENTA + EscapeSequences.SET_TEXT_COLOR_WHITE + " ").
+//                    append(row).append(" ").append(EscapeSequences.RESET_BG_COLOR);
+//            for (int col = start; col <= 8 && col >= 1; col += inc) {
+//                //figures out if square needs to be shaded or not
+//                for (ChessMove move : moves) {
+//                    if (Objects.equals(move.getEndPosition(), new ChessPosition(row, 9 - col))) {
+//                        determineHighlight();
+//                    }
+//                }
+//
+//                drawing.append(squareShade).append(getPieceString(board, row, 9 - col)).append(EscapeSequences.RESET_BG_COLOR);
+//                changeShade();
+//            }
+//            //draws the right border
+//            drawing.append(borderColor).append(" ").append(row).append(" ").append(EscapeSequences.SET_BG_COLOR_BLACK).append("\n");
+//            changeShade();
+//        }
+//        drawing.append(borderColor);
+//        drawing.append(drawTopOrBottom(perspective));
+//        drawing.append(EscapeSequences.RESET_BG_COLOR);
+//
+//
+//        return drawing.toString();
     }
 
 }
